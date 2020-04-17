016f85072eb21adfad4664546a7e13dfb8e7bc09
john.saxton <jssaxton86@gmail.com>
2020-04-16 18:53:11 -0700

    Just testing s3 stuff

--- api/v1/index.js
+++ api/v1/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const routes = [];
 routes.push(require('./headers'));
 routes.push(require('./pokemon'));
+routes.push(require('./s3'));
 
 const router = express.Router();
 
---
+++ api/v1/s3.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const { S3 } = require("lib/service/aws");
+
+router.get("/idol/:idol", (req, res, next) => {
+    S3.getImage(req.query.number).pipe(res);    
+});
+
+exports = module.exports = router;
