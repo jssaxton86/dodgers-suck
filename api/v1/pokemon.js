5c7e1437bd32f799434d4929420d1d4dea526b65
john.saxton <jssaxton86@gmail.com>
2020-04-16 21:42:21 -0700

    Update pokemon route.

--- api/v1/pokemon.js
+++ api/v1/pokemon.js
@@ -2,7 +2,11 @@ const router = require('express').Router();
 const { Pokemon } = require("lib/service/pokemon");
 
 router.get("/pokemon", (req, res, next) => {
-    Pokemon.getPokemon(req.query.number).pipe(res);    
+    Pokemon.getPokemon(req.query.number)
+    .on( 'response', (res) => {
+        delete res.headers['cache-control'];
+    })
+    .pipe(res)
 });
 
 exports = module.exports = router;
--- api/v1/s3.js
+++ api/v1/s3.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { S3 } = require("lib/service/aws");
 
 router.get("/idol/:idol", (req, res, next) => {
-    S3.getImage(req.query.number).pipe(res);    
+    S3.getImage(req.params.idol).pipe(res);    
 });
 
 exports = module.exports = router;
