0d5d489dec3c05f8fc041ad62638eab15d488f9e
john.saxton <jssaxton86@gmail.com>
2020-04-16 18:29:34 -0700

    Small update to the query

--- api/v1/pokemon.js
+++ api/v1/pokemon.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Pokemon } = require("lib/service/pokemon");
 
 router.get("/pokemon", (req, res, next) => {
-    Pokemon.getPokemon(req.params.number).pipe(res);    
+    Pokemon.getPokemon(req.query.number).pipe(res);    
 });
 
 exports = module.exports = router;
